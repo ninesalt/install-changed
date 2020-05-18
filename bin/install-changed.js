50441cbda165836565d9bd70c1e47221a8676b08
thdk <t.dekiere@gmail.com>
2020-05-18 21:38:28 +0200

    Add install options
    'hashFileName' and 'command' can now be specified for both cli and api version.
    users may now specify --command="npm ci" if they would prefere that over "npm install"
    they can also change the filename of the file that contains the hash with --hashFileName
    
    api function watchFile now has a options argument: { hashFileName: string | undefined, command: string | function }
    
    Fixes ninesalt/install-changed#7

--- bin/install-changed.js
+++ bin/install-changed.js
@@ -1,4 +1,40 @@
 #!/usr/bin/env node
 
-let watcher = require('../lib/main')
-watcher()
+const watcher = require('../lib/main')
+
+const optionsFromArgs = () => {
+    const args = process.argv.slice(2)
+    const options = {}
+
+
+    for (let argIndex = 0; argIndex < args.length; argIndex++) {
+        const arg = args[argIndex].split('=')
+        const argName = arg[0]
+        const argValue = arg.length > 0 && arg[1]
+        switch (argName) {
+            case '--command':
+                if (argValue === false) {
+                    console.warn('--command requires a value. Skipping argument...')
+                }
+                else {
+                    options.command = argValue
+                }
+                break
+            case '--hashFileName':
+                if (argValue === false) {
+                    console.warn('--hashFileName requires a value. Skipping argument...')
+                }
+                else {
+                    options.hashFileName = argValue
+                }
+                break
+            default:
+                console.warn(`Unknown argument: '${argName}'`)
+                break
+        }
+    }
+
+    return options
+}
+
+watcher(optionsFromArgs())
