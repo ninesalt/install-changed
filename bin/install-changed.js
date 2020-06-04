91a022943aa393327c78a8cdb6ac283a414f0f69
thdk <t.dekiere@gmail.com>
2020-06-04 10:44:48 +0200

    Use commander to parse cli arguments

--- bin/install-changed.js
+++ bin/install-changed.js
@@ -1,40 +1,16 @@
 #!/usr/bin/env node
 
+const { program } = require('commander')
+
 const watcher = require('../lib/main')
 
-const optionsFromArgs = () => {
-    const args = process.argv.slice(2)
-    const options = {}
+program
+  .option('--install-command [command]', 'The command to run when dependencies need to be installed/updated')
+  .option('--hash-filename [filename]', 'Filename where hash of dependencies will be written to')
 
+program.parse(process.argv)
 
-    for (let argIndex = 0; argIndex < args.length; argIndex++) {
-        const arg = args[argIndex].split('=')
-        const argName = arg[0]
-        const argValue = arg.length > 0 && arg[1]
-        switch (argName) {
-            case '--command':
-                if (argValue === false) {
-                    console.warn('--command requires a value. Skipping argument...')
-                }
-                else {
-                    options.command = argValue
-                }
-                break
-            case '--hashFileName':
-                if (argValue === false) {
-                    console.warn('--hashFileName requires a value. Skipping argument...')
-                }
-                else {
-                    options.hashFileName = argValue
-                }
-                break
-            default:
-                console.warn(`Unknown argument: '${argName}'`)
-                break
-        }
-    }
-
-    return options
-}
-
-watcher(optionsFromArgs())
+watcher({
+  installCommand: program.installCommand,
+  hashFilename: program.hashFilename
+})
